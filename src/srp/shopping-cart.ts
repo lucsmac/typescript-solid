81a068bd658c3286e8adf0f974c572a278b5f575
Lucas Macedo <lucasmdc57@gmail.com>
2021-05-06 16:19:01 -0300

    creating a cart without solid principles

---
+++ src/srp/shopping-cart.ts
@@ -0,0 +1,67 @@
+type CartItem = { name: string; price: number };
+type OrderStatus = 'open' | 'closed';
+
+export class ShoppingCart {
+  private readonly _items: CartItem[] = [];
+  private _orderStatus: OrderStatus = 'open';
+
+  get items(): Readonly<CartItem[]> {
+    return this._items;
+  }
+
+  get orderStatus(): Readonly<OrderStatus> {
+    return this._orderStatus;
+  }
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2);
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  sendMessage(msg: string): void {
+    console.log('Mensagem enviada:', msg);
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso...');
+  }
+
+  clear(): void {
+    console.log('Carrinho de compras foi limpo');
+    this._items.length = 0;
+  }
+
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.sendMessage(`Seu pedido com total de R$${this.total()} foi recebido.`);
+    this.saveOrder();
+    this.clear();
+  }
+}
+
+const shoppingCart = new ShoppingCart();
+shoppingCart.addItem({ name: 'Camiseta', price: 49.9 });
+shoppingCart.addItem({ name: 'Bermuda', price: 79.9 });
+shoppingCart.addItem({ name: 'Tênis', price: 129.9 });
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+shoppingCart.checkout();
